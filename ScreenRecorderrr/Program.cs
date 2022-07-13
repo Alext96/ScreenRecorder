db7555abbe0fedc9feb0609a7f2a8606d574c52f
Alext96 <alextarberg96@gmail.com>
2022-07-14 00:28:41 +0200

    Add files via upload

---
+++ ScreenRecorderrr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScreenRecorder;
+
+namespace screen_recorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            var num = rnd.Next();
+            var rec = new Recorder(new RecorderParams("out" + num + ".avi", 10, SharpAvi.KnownFourCCs.Codecs.MotionJpeg, 70));
+            Console.WriteLine("Press any key to Stop...");
+            Console.ReadKey();
+
+            // Finish Writing
+            rec.Dispose();
+        }
+    }
+}
